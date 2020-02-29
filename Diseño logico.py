025c3704023a00ff38b1eacdefc5a875b75c1199
leinad3008 <61519368+leinad3008@users.noreply.github.com>
2020-02-28 19:17:40 -0600

    Add files via upload

--- Diseño logico.py
+++ Diseño logico.py
@@ -26,10 +26,99 @@ def ToBin(num, let): #pasa el numero octal a binario
         i=i-1
     return x
         
-def ToHex(bi):    #pasa el numero binario a Hexa
+def hexa(bina):
+    largo=len(bina)
+   
+    if largo%4==0:
+        lista1=[]
+        cont=0
+        contf=0
+        
+        while cont<largo:
+            aux=""
+            while (contf<4):
+                aux=aux+(bina[cont])
+                contf+=1
+                cont+=1
+            
+            lista1.append(aux)
+            contf=0
+        global aux1
+        aux1=[]
+        for n in range(0,len(lista1)):
+            if lista1[n]=="0000":
+                aux1.append("0")
+            elif lista1[n]=="0001":
+                aux1.append("1")
+            elif lista1[n]=="0010":
+                aux1.append("2")
+            elif lista1[n]=="0011":
+                aux1.append("3")
+            elif lista1[n]=="0100":
+                aux1.append("4")
+            elif lista1[n]=="0101":
+                aux1.append("5")
+            elif lista1[n]=="0110":
+                aux1.append("6")
+            elif lista1[n]=="0111":
+                aux1.append("7")
+            elif lista1[n]=="1000":
+                aux1.append("8")
+            elif lista1[n]=="1001":
+                aux1.append("9")
+            elif lista1[n]=="1010":
+                aux1.append("A")
+            elif lista1[n]=="1011":
+                aux1.append("B")
+            elif lista1[n]=="1100":
+                aux1.append("C")
+            elif lista1[n]=="1101":
+                aux1.append("D")
+            elif lista1[n]=="1110":
+                aux1.append("E")
+            elif lista1[n]=="1111":
+                aux1.append("F")
+            else:
+                print("Numero invalido")
+       
+        
+    else: #Correcion en caso de que el numero binario no tenga todos los ceros
+        nuevo="0"
+        nuevo=nuevo+bina
+        
+        largo=len(nuevo)
+        hexa(nuevo)
+    Hexa=""
+    for ele in range(0,len(aux1)):
+        Hexa=Hexa+(aux1[ele])
+    
+    return Hexa
     
     
     
+    
+def Nrz(bina):
+    lista=["0"]
+    Datos_Nrz=""
+    contl=1
+    cont=0
+    while cont<len(bina):
+        
+        if bina[cont]=='0':
+            lista.append(lista[contl-1])
+        else:
+            if lista[contl-1]=="0":
+                lista.append("1")
+            else:
+                lista.append("0")
+        cont+=1
+        contl+=1
+        
+    for ele in range(1,len(lista)):
+        Datos_Nrz=Datos_Nrz+lista[ele]
+    return Datos_Nrz
+
+
 
 def submenu(): #submenu final que permite terminar el programa o continuar 
     print("1- digitar un nuevo número")
@@ -81,12 +170,16 @@ def menu():
         
         valid(Octal, Largo) #función valid
 
-        NumBin= int(ToBin(Octal, Largo)) #funcion ineficiente para pasar a binario y lo guarda en la una variable tipo int 
+        NumBin= str(ToBin(Octal, Largo)) #funcion ineficiente para pasar a binario y lo guarda en la una variable tipo int 
 
-        NumHexa= ToHex(NumBin)
+        NumHexa= hexa(NumBin)
         
-        print(NumBin)
-        print(NumHexa)
+        print("El número",Octal,"en binario es:",NumBin)
+        print("El número",Octal,"en hexadecimal es:",NumHexa)
+
+        dato=Nrz(NumBin)
+        print("Dato en Nrz:",dato)
+        
         submenu()
         
     except ValueError:
